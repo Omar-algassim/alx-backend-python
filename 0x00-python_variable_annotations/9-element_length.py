a39e849a5af7ca0581c15a0a9bf98dcb284ef11a
Omar-Algassim <omarggg2@gamil.com>
2024-07-05 04:42:42 +0300

    add docmuntation and fix pycodestyle

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
+"""get iterable and return list of tuples
+with each element and its length"""
 from typing import Sequence, Tuple, List, Iterable
 
  
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """function that takes a list of strings and returns a list of tuples"""
     return [(i, len(i)) for i in lst]
