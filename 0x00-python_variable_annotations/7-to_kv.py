6fda6c9115a9272e0ee9c80061757984b4ce0557
Omar-Algassim <omarggg2@gamil.com>
2024-07-05 03:28:55 +0300

    using Union from typing

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """input a string and an int/float to return a tuple"""
+from typing import Union
 
-
-def to_kv(k: str, v: [int | float]) -> tuple:
+def to_kv(k: str, v: Union[int, float]) -> tuple:
     """to_kv: takes a string k and an int/float v as arguments
     and returns a tuple"""
     return (k, v ** 2)
