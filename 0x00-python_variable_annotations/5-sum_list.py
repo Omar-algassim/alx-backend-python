fd6c1be7b0cd879b208d2f03e70386786667b880
Omar-Algassim <omarggg2@gamil.com>
2024-07-05 03:40:18 +0300

    using typing List

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """summation of a list of floats"""
+from typing import List
 
-
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """sum_list: takes a list of floats as argument and returns their sum"""
     return sum(input_list)
