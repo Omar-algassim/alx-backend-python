9ddb57c91167c48d1495764b2618ff0a6ed36703
Omar-Algassim <omarggg2@gamil.com>
2024-07-05 03:30:30 +0300

    add annotate to tuple

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """input a string and an int/float to return a tuple"""
-from typing import Union
+from typing import Union, Tuple
 
-def to_kv(k: str, v: Union[int, float]) -> tuple:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """to_kv: takes a string k and an int/float v as arguments
     and returns a tuple"""
     return (k, v ** 2)
