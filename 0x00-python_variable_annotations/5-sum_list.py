0bf3f35cbca7f52d4a3327a1455141ca31b58588
Omar-Algassim <omarggg2@gamil.com>
2024-07-05 03:08:02 +0300

    implement sum_list function for calculating the sum of a list of floats

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""summation of a list of floats"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """sum_list: takes a list of floats as argument and returns their sum"""
+    return sum(input_list)
