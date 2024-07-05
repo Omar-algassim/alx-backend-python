c16f00d53f1ef4bdcd54e264be52ae8f426db957
Omar-Algassim <omarggg2@gamil.com>
2024-07-05 03:19:29 +0300

    using List and Union

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """summation of a list of floats and integers"""
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: list[int | float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """sum_mixed_list: takes a list of floats and integers as argument and returns their sum"""
     return sum(mxd_lst)
