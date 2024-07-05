657dd7a6e22b5d14083e5c66940ccad16806d7e7
Omar-Algassim <omarggg2@gamil.com>
2024-07-05 03:16:21 +0300

    update format for annotate

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -2,6 +2,6 @@
 """summation of a list of floats and integers"""
 
 
-def sum_mixed_list(mxd_lst: list[int, float]) -> float:
+def sum_mixed_list(mxd_lst: list[int | float]) -> float:
     """sum_mixed_list: takes a list of floats and integers as argument and returns their sum"""
     return sum(mxd_lst)
