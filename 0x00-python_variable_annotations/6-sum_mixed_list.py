677d75e099f2679513ea6ba9225d911c06147df8
Omar-Algassim <omarggg2@gamil.com>
2024-07-05 03:10:56 +0300

    summation of mix list of integer and float

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""summation of a list of floats and integers"""
+
+
+def sum_mixed_list(mxd_lst: list[int, float]) -> float:
+    """sum_mixed_list: takes a list of floats and integers as argument and returns their sum"""
+    return sum(mxd_lst)
