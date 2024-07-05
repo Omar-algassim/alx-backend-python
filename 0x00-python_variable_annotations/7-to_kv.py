7988523d62d59845c296998f4577f1a70afc6797
Omar-Algassim <omarggg2@gamil.com>
2024-07-05 03:31:23 +0300

    fix pycodestyle

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -2,6 +2,7 @@
 """input a string and an int/float to return a tuple"""
 from typing import Union, Tuple
 
+
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """to_kv: takes a string k and an int/float v as arguments
     and returns a tuple"""
