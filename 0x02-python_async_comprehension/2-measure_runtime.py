8acc66cde02a08fb75f35f77360f194c84ed5864
Omar-Algassim <omarggg2@gamil.com>
2024-07-09 19:33:24 +0300

    change the compeerhension

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -6,9 +6,8 @@ async_comprehension = __import__("1-async_comprehension").async_comprehension
 
 async def measure_runtime() -> float:
     """clculate the time async_comprehension spend"""
-    comprhension = asyncio.create_task(async_comprehension())
+    comprhension = [(async_comprehension()) for i in range(4)]
     start = time.perf_counter()
-    for i in range(4):
-        await asyncio.gather(comprhension)
+    await asyncio.gather(*comprhension)
     end = time.perf_counter()
     return end - start
