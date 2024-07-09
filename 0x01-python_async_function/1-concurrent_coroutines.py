a7e782bad4811f4de4f9ce56fc52a6e954082018
Omar-Algassim <omarggg2@gamil.com>
2024-07-09 05:20:47 +0300

    pycodestyle fix

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 """concurrent module"""
-wait = __import__("0-basic_async_syntax").wait_random
 import asyncio
 from typing import List
+wait = __import__("0-basic_async_syntax").wait_random
 
 
-async def wait_n(n : int, max_delay : int) -> List[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """run wait_random n times and return waits time list"""
     wait_list = [await wait(max_delay) for _ in range(n)]
     return sorted(wait_list)
-    
