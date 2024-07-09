19e9a0b67abaeeb527fe716d277d31c35dd20201
Omar-Algassim <omarggg2@gamil.com>
2024-07-09 03:18:01 +0300

    basic async function

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""async function"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay=10.0):
+    """wait random second and return it"""
+    num = random.uniform(0, max_delay)
+    await asyncio.sleep(num)
+    return num
