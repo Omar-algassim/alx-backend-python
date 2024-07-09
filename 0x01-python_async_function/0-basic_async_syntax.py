34b5033599e2daaa245e96f30cb03f7dd012a52e
Omar-Algassim <omarggg2@gamil.com>
2024-07-09 03:29:24 +0300

    add type anntate

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,9 +4,8 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay=10.0):
+async def wait_random(max_delay: float = 10) -> float:
     """wait random second and return it"""
     num = random.uniform(0, max_delay)
     await asyncio.sleep(num)
     return num
-
