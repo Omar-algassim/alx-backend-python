ab7c16fb138055fe614cd701fc62d0b1670e5d65
Omar-Algassim <omarggg2@gamil.com>
2024-07-09 03:33:18 +0300

    change max delay to int

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,7 +4,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay: float = 10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """wait random second and return it"""
     num = random.uniform(0, max_delay)
     await asyncio.sleep(num)
